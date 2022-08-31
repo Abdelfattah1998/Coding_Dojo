64b2949ae78c4bc196bb562f45fd388c65697654
abdelfattah1998 <abdelfattah2202@gmail.com>
2022-08-31 11:40:52 +0200

    profile

---
+++ WebFundamentals/JS/Making Connections/Making Connections/profile.js
@@ -0,0 +1,28 @@
+parent1=document.querySelector("#parent")
+user1=document.querySelector("#user1");
+user2=document.querySelector("#user2");
+button1=document.querySelector("#button1");
+button2=document.querySelector("#button2");
+nam=document.querySelector("#name");
+
+var count1 =2;
+var count2 =500;
+function accept1(){
+    parent1.removeChild(user1);
+    count1--;
+    count2++;
+    button1.innerText=count1;
+    button2.innerText=count2;
+    console.log("accept1");
+}
+function accept2(){
+    parent1.removeChild(user2);
+    count1--;
+    count2++;
+    button1.innerText=count1;
+    button2.innerText=count2;
+    console.log("accept2");
+}
+function editprofile(){
+    nam.innerText="Any Name";
+}
