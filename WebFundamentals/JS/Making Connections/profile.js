d2aa348bbe8a56ddf3f89ac3286f344730edc068
abdelfattah1998 <abdelfattah2202@gmail.com>
2022-08-31 13:11:53 +0200

    Making Connections

--- WebFundamentals/JS/Making Connections/profile.js
+++ WebFundamentals/JS/Making Connections/profile.js
@@ -23,6 +23,18 @@ function accept2(){
     button2.innerText=count2;
     console.log("accept2");
 }
+function reject1(){
+    parent1.removeChild(user1);
+    count1--;
+    button1.innerText=count1;
+    console.log("accept1");
+}
+function reject2(){
+    parent1.removeChild(user2);
+    count1--;
+    button1.innerText=count1;
+    console.log("accept2");
+}
 function editprofile(){
     nam.innerText="Any Name";
 }
