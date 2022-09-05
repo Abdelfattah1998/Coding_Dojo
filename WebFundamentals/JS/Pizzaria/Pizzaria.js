8adaa4c82d1f4dc17e0aa2792393d709e1ca90d3
abdelfattah1998 <abdelfattah2202@gmail.com>
2022-09-05 15:46:48 +0200

    modified

--- WebFundamentals/JS/Pizzaria/Pizzaria.js
+++ WebFundamentals/JS/Pizzaria/Pizzaria.js
@@ -6,12 +6,11 @@ function PizzaFactory(crustType, sauceType, cheeses, toppings) {
  Pizza.toppings = toppings;
     return Pizza;
 }
-// var s ;
-var s0 = PizzaFactory("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
-var s1 = PizzaFactory("hand tossed", "marina", ["mozzarella","feta"], ["mushrooms", "olives", "onions"]);
-var s2= PizzaFactory("Stuffed Crust", "Buffalo", "provolone", ["tomato", "pepperoni", "pepper"]);
-var s3 = PizzaFactory("Cheese Crust Pizza", "Garlic Ranch", "Cheddar", ["mushrooms", "fried onions", "tomato","pepper"]);
+var s =[0,1,2,3];
+ s[0] = PizzaFactory("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]);
+ s[1] = PizzaFactory("hand tossed", "marina", ["mozzarella","feta"], ["mushrooms", "olives", "onions"]);
+ s[2]= PizzaFactory("Stuffed Crust", "Buffalo", "provolone", ["tomato", "pepperoni", "pepper"]);
+ s[3]= PizzaFactory("Cheese Crust Pizza", "Garlic Ranch", "Cheddar", ["mushrooms", "fried onions", "tomato","pepper"]);
 
-s=[s0,s1,s2,s3];
-s[4]=PizzaFactory(s[Math.ceil((Math.random *3))].crustType, s[Math.ceil((Math.random *3))].sauceType, s[Math.ceil((Math.random *3))].cheeses, s[Math.ceil((Math.random *3))].toppings);
+s[4]=PizzaFactory(s[Math.ceil((Math.random() *3))].crustType, s[Math.ceil((Math.random() *3))].sauceType, s[Math.ceil((Math.random() *3))].cheeses, s[Math.ceil((Math.random() *3))].toppings);
 console.log(s[4]);
