00571b02caa55eedcb3d050fc1d7f0f6f1c984b7
abdelfattah1998 <abdelfattah2202@gmail.com>
2022-12-07 15:08:40 +0200

    First React With JSX

---
+++ MERN/React/First_JSX/appme/src/App.js
@@ -0,0 +1,34 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h2>Things I need to do</h2>
+
+      <ul>
+        <li>Learn React</li>
+        <li>Climb mountain </li>
+        <li>Jump off the mountain</li>
+      </ul>
+      
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
