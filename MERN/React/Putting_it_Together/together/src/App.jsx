55d92f5eb8abac97634abfd3b59e8810f6d22462
abdelfattah1998 <abdelfattah2202@gmail.com>
2022-12-08 14:15:05 +0200

    Putting it Together

---
+++ MERN/React/Putting_it_Together/together/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import SomeClassComponent from './components/SomeClassComponent';
+function App () {    
+  return(
+    <>
+    <SomeClassComponent firstName="Doe" lastName="Jane" age={45} hairColor={"Black"}></SomeClassComponent>
+    <SomeClassComponent firstName="Smith" lastName="John" age={45} hairColor={"red"}></SomeClassComponent>
+    <SomeClassComponent firstName="Fillmore" lastName="Millard" age={45} hairColor={"blonde"}></SomeClassComponent>
+    <SomeClassComponent firstName="Smith" lastName="Maria" age={45} hairColor={"Brown"}></SomeClassComponent>
+    </>
+  );
+}
+
+export default App;
---
+++ MERN/React/Putting_it_Together/together/src/Components/SomeClassComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class SomeClassComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.age
+        };
+    }
+    render() {
+        return (
+            <div>
+                <div>
+                <h1>My name is { this.props.firstName } { this.props.lastName }</h1>
+                    <h3>Age:{this.state.age}</h3>
+                    <h3>Hair Color: {this.props.hairColor}</h3>
+                    <button onClick={ () => { this.setState({age:this.state.age+1})} }>Increment Age</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SomeClassComponent;
