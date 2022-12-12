213d7f00235d0069009ecf99dc534959d52802a7
abdelfattah1998 <abdelfattah2202@gmail.com>
2022-12-12 03:59:52 +0200

    box

--- MERN/React/Box_Generator/boxgenerator/src/Components/Boxes.js
+++ MERN/React/Box_Generator/boxgenerator/src/Components/Boxes.js
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 
-const Box = ({ color }) => (
-    <div
-      style={{
-        backgroundColor: color,
-        width: "100px",
-        height: "100px",
-        display: "inline-block",
-        margin: "0.5em"
-      }}
-    />
-  );
+// const Box = ({ color }) => (
+
+//   );
 
 const Boxer = (props) => {
   return (
     <div>
       <div>
         {props.color.map(x => (
-          <Box key={x} color={x} />
+    <div key={x}
+    style={{
+      backgroundColor: x,
+      width: "100px",
+      height: "100px",
+      display: "inline-block",
+      margin: "0.5em"
+    }}
+  />
         ))}
       </div>
     </div>
